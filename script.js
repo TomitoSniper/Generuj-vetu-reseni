7abb35366aac193966ec032ec45b2fd1077765ad
Tomáš Hájek <tomas.hajek2007@seznam.cz>
2025-02-10 20:34:14 +0100

    Hotovo

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-// Autor: Martin Procházka, 2024.01
-// Popis: Skript generuje náhodné věty složené z náhodného počtu slov složených z náhodného počtu písmen
-
-
-// Funkce generujSlovo generuje náhodné slovo o zadané délce
-function generujSlovo(pocetPismen)
+function generujSlovo(pocetPismen) 
 {
     let pismena = "abcdefghijklmnopqrstuvwxyz";
     let i = 0;
     let slovo = "";
-    while (i < pocetPismen) {
-        let indexPismena = Math.floor(Math.random() * pismena.length);
+    while(i<pocetPismen) {
+        let indexPismena = Math.floor(Math.random()*pismena.length);
         slovo += pismena[indexPismena];
         i++;
     }
     return slovo;
 }
-
-// Funkce generujVetu generuje náhodnou větu o zadaném počtu slov
-function generujVetu(pocetSlov)
+function generujVetu(pocetSlov) 
 {
     let i = 0;
     let veta = "";
-    while (i < pocetSlov) {
-        let slovo = generujSlovo(Math.random() * 10 + 1);
+    while(i<pocetSlov) {
+        let slovo = generujSlovo(Math.random()*10+1);
         veta += slovo + " ";
         i++;
     }
+    veta = veta.trim();
+    veta = veta.charAt(0).toUpperCase() + veta.slice(1);
+    veta += ".";
     return veta;
 }
-
-// Generování deseti vět
 let i = 0;
-while (i<10) {
-    console.log(generujVetu(Math.random() * 6 + 1));
-    i++;
+while(i<10) {
+    let pocetSlov = Math.floor(Math.random()*6)+1;
+    console.log(generujVetu(pocetSlov));
+    i++
 }
